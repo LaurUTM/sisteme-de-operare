11ec51a358c2d64ac601b8433b9357c73084d8d9
Natalia <sednevnatalia.2023@gmail.com>
2026-09-16 10:19:48 +0300

    Mesaj

--- SmartTimerManager/src/main/java/md/utm/smarttimermanager/HelloController.java
+++ SmartTimerManager/src/main/java/md/utm/smarttimermanager/HelloController.java
@@ -366,7 +366,7 @@ public class HelloController {
                     secunda < 0 || secunda > 59) {
 
                 exactStatusLabel.setText(
-                        "● Time nevalid"
+                        "● Timp nevalid"
                 );
 
                 return;
